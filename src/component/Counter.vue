8bba5bfcc9ba2e50da360a2336bbe1b3b785afdf
orange <3441602787@qq.com>
2020-07-30 14:49:40 +0800

    test -vue

---
+++ src/component/Counter.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: luohongwen
+ * @Date: 2020-07-30 14:11:38
+ * @LastEditTime: 2020-07-30 14:11:49
+--> 
+<template>
+    <div>
+        {{ count }}
+        <button @click="increment">自增</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            count: 0,
+        }
+    },
+
+    methods: {
+        increment() {
+            this.count++
+        },
+    },
+}
+</script>
--- src/sum.js
+++ src/sum.js
@@ -3,10 +3,34 @@
  * @version:
  * @Author: luohongwen
  * @Date: 2020-07-15 12:47:59
- * @LastEditTime: 2020-07-15 12:48:37
+ * @LastEditTime: 2020-07-17 12:48:58
  */
+import axios from 'axios';
 export default {
     sum(a, b) {
         return a + b;
+    },
+    getAuthor() {
+        return {
+            name: 'LITANGHUI',
+            age: 24,
+        }
+    },
+    getIntArray(num) {
+        if (!Number.isInteger(num)) {
+            throw Error('"getIntArray"只接受整数类型的参数');
+        }
+
+        let result = [];
+        for (let i = 0, len = num; i < len; i++) {
+            result.push(i);
+        }
+
+        return result;
+    },
+    fetchUser() {
+        return axios.get('http://jsonplaceholder.typicode.com/users/1')
+            .then(res => res.data)
+            .catch(error => console.log(error));
     }
 }
