37933ecd2042f71132f4631b8060e9a94483606b
orange <3441602787@qq.com>
2020-08-15 14:23:31 +0800

    up

--- src/sum.js
+++ src/sum.js
@@ -5,8 +5,14 @@
  * @Date: 2020-07-15 12:47:59
  * @LastEditTime: 2020-07-15 12:48:37
  */
+import axios from 'axios';
 export default {
     sum(a, b) {
         return a + b;
+    },
+    fetchUser() {
+        return axios.get('http://jsonplaceholder.typicode.com/users/1')
+            .then(res => res.data)
+            .catch(error => console.log(error));
     }
 }
--- test/sum.test.js
+++ test/sum.test.js
@@ -11,3 +11,15 @@ import functions from '../src/sum.js';
 test('sum(2 + 2) 等于 4', () => {
     expect(functions.sum(2, 2)).toBe(4);
 })
+test('fetchUser() 可以请求到一个含有name属性值为Leanne Graham的对象', () => {
+    expect.assertions(1);
+    return functions.fetchUser()
+        .then(data => {
+            expect(data.name).toBe('Leanne Graham');
+        });
+})
+test('fetchUser() 可以请求到一个用户名字为Leanne Graham', async () => {
+    expect.assertions(1);
+    const data = await functions.fetchUser();
+    expect(data.name).toBe('Leanne Graham')
+})
